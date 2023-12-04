5ea51b43132ffc24c467ed1a9c9879e184130c8f
Miguel <miguelaf71@hotmail.com>
2023-12-04 20:12:33 +0100

    separación iconos, fondo blanco en modales

--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -54,15 +54,15 @@ const notify= (status) =>{
     <div >
 
       <p>Email: <span className="perfil__user-data">{userData.email}</span></p>
-      <p>Usuario:<span className="perfil__user-data">{userData.username}</span> </p>
+      <p>Usuario: <span className="perfil__user-data">{userData.username}</span> </p>
       {userData.avatar && (
         <div className="perfil-avatar">
           <img className="header_avatar" src={userData.avatar} alt="avatar" />
         </div>
       )}
-      <p>Notificar novo evento: {userData.newevent ? (<span className="perfil__user-data">Sí</span>):(<span className="perfil__user-data">No</span>)}</p>
+      <p>Email con novos eventos: {userData.newevent ? (<span className="perfil__user-data green">Si</span>):(<span className="perfil__user-data red">Non</span>)}</p>
 
-      <p>Mandar eventos da semana: {userData.newsletter ? (<span className="perfil__user-data">Sí</span>):(<span className="perfil__user-data">No</span>)}</p>
+      <p>Email con eventos da semana: {userData.newsletter ? (<span className="perfil__user-data green">Si</span>):(<span className="perfil__user-data red">Non</span>)}</p>
       <div>
               {userData && userData.role === 2 && (<>
                 <Link to="/crear-evento">
