0d7c3a26a79ae8487f2b49b0745873bba5a93da7
Miguel <miguelaf71@hotmail.com>
2023-12-11 15:03:11 +0100

    border en proximos eventos

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import "./Home.css";
 import EventosList from "../../components/EventosList/EventosList";
-import { BsArrowUp, BsFillArrowUpCircleFill, BsInfoCircleFill } from "react-icons/bs";
+import {
+  BsArrowUp,
 
+} from "react-icons/bs";
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="header-listado">
-      <div className="black-border">
-        <h1>PROXIMOS EVENTOS</h1>
-        {/* <p>Preme <BsInfoCircleFill style={{ color: 'var(--icon-color)' }}/> para ver a ficha completa</p> */}
-        </div>
+      <div className="listado-border">
+      <h1 className="listado-text">PROXIMOS EVENTOS</h1>
       </div>
+      <div className="listado-container">
+        <h1 className="listado-text">PROXIMOS EVENTOS</h1>
+      </div> 
       <EventosList />
-      <BsArrowUp className="scroll-up" onClick={()=>{window.scrollTo(0,0)}} style={{ strokeWidth: '1.3' }}/>
+      <BsArrowUp
+        className="scroll-up"
+        onClick={() => {
+          window.scrollTo(0, 0);
+        }}
+      />
     </div>
   );
 };
